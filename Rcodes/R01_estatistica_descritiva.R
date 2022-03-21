84ff9ba49047face5009b9a227b13ced7e026370
Adelino Pinheiro Silva <adelinocpp@yahoo.com>
2022-03-21 18:50:22 -0300

    Num frames

--- Rcodes/R01_estatistica_descritiva.R
+++ Rcodes/R01_estatistica_descritiva.R
@@ -1,7 +1,5 @@
 cat("\014")
 rm(list = setdiff(ls(), lsf.str()))
-library(qcc) # pareto
-
 # -----------------------------------------------------------------------------
 data_lemas <- read.table("../Dados/lexporbr_alfa_lemas_txt.txt", header = TRUE, 
                         sep = "\t",dec = ",",quote = "\"")
@@ -9,38 +7,17 @@ head(data_lemas)
 dim(data_lemas)
 summary(data_lemas)
 # -----------------------------------------------------------------------------
-# vec_nb_letras = data_lemas$nb_letras
-# vec_cat_gram = data_lemas$cat_gram
-# cat_gram_unique = unique(vec_cat_gram)
-
-n_data = length(data_lemas$nb_letras)
-std_data = sd(data_lemas$nb_letras)
-# n_scott = round(3.5*std_data/(n_data^(1/3)))
-
 png(file = "../Imagens/histograma.png",width = 864, height = 486, units = "px")
 hist(data_lemas$nb_letras,main="Histograma do numero de letras em cada ocorrência",
      breaks=40,xlab = "número de letras", ylab="Ocorrências",col = "blue2",
      border="white")
 dev.off()
-
+# -----------------------------------------------------------------------------
 png(file = "../Imagens/Box_plot.png",width = 864, height = 486, units = "px")
 boxplot(data_lemas$nb_letras ~ cat_gram, data = data_lemas, ylab = "Número de letras", col = "blue2",
         border="black")
 dev.off()
-
-# yarrr::pirateplot(formula = nb_letras ~ cat_gram, # dv is weight, iv is Diet
-#                   data = data_lemas,
-#                   main = "Numero letras por categoria",
-#                   xlab = "Categoria",
-#                   ylab = "Numero de letras")
-
+# -----------------------------------------------------------------------------
 stem(data_lemas$nb_letras[1:300])
-
-
+# -----------------------------------------------------------------------------
 tab <- table(data_lemas$cat_gram, data_lemas$nb_letras)
-# pareto.chart(tab)
-# pirateplot(dv.name = nb_letras,
-#            iv.name = cat_gram,
-#            data = data_lemas,
-#            main = "Numero letras por categoria"
-# )
